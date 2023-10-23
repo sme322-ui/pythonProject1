a0df5e88e0fd5add5cf9c7b2af5da86bb572b703
sme322 <xiemengy9@gmail.com>
2023-10-23 21:22:37 +0800

    線性回歸

--- graph.py
+++ graph.py
@@ -1,27 +1,34 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import make_regression
+from matplotlib import pyplot
+from sklearn.datasets import make_blobs
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+import tensorflow as tf
+model = LinearRegression()
+import keras.backend as K
 
-cancer = load_breast_cancer()
+import numpy as np
+heights = [[1.6],[1.65],[1.7],[1.73],[1.8]]
+weights = [[60],[65],[72.3],[75],[80]]
+model.fit(X=heights,y=weights)
+pyplot.plot(heights,weights,'k.')
+pyplot.title('Weight plotted against heights')
+pyplot.xlabel('Observe value')
+pyplot.ylabel('expect times')
+pyplot.axis([1.5,1.85,50,90])
 
-X = []
-for target in range(2):
-    X.append([[], [], []])
-    for i in range(len(cancer.data)):
-        if cancer.target[i] == target:
-            X[target][0].append(cancer.data[i][0])
-            X[target][1].append(cancer.data[i][1])
-            X[target][2].append(cancer.data[i][2])
+pyplot.grid(True)
+pyplot.plot(heights,model.predict(heights),color='r')
+print('Residual sum of squares: %.2f' % np.sum((weights - model.predict(heights))**2))
 
-colours = ("r", "b")
-fig = plt.figure(figsize=(33, 28))
-ax = fig.add_subplot(111, projection='3d')
-for target in range(2):
-    ax.scatter(X[target][0],
-               X[target][1],
-               X[target][2],
-               c=colours[target])
-    ax.set_xlabel("mean radius")
-    ax.set_ylabel("mean texture")
-    ax.set_zlabel("mean perimeter")
-    plt.show();
+pyplot.show()
+print('回歸截距: w0={}'.format(model.intercept_[0],2))
+print('回歸系数: w1={}'.format(model.coef_[0][0],2))
+message = tf.constant("Hello World!")
+tf.compat.v1.disable_eager_execution()
+hello=tf.constant('Hello,TensorFlow')
+config=tf.compat.v1.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.9
+sess=tf.compat.v1.Session(config=config)
+print(sess.run(hello))
+print(K.epsilon())
